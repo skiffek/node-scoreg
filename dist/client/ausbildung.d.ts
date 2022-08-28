c91e84dba67da37f639d7cf67d2a72d65e2d906d
Wolfgang Sassik <wolfgang@sassik.at>
2022-08-28 14:41:43 +0200

    Drop dist from git

--- dist/client/ausbildung.d.ts
+++
@@ -1,5 +0,0 @@
-import Client from "../client";
-export default class Ausbildung extends Client {
-    findScoutIdsForOrganization(): Promise<string[]>;
-    findMemberByScoutId(scoutId: string): Promise<any>;
-}
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default } from "./client";
-export { default as MemberV2 } from "./client/member-v2";
-export { default as Ausbildung } from "./client/ausbildung";
